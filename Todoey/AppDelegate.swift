8d118eded7dab87e407eb83eb73898f6e1494f31
Lalith kumar <lalith@laliths-macbook-air.local>
2018-09-15 12:12:33 +0530

    Data persisted using UserDefault

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -15,16 +15,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        //when app gets loaded up / before view did load ( this happens when goes into the background)
+       // print("1st call")
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
+        // when the user receives a call (suppose you are eneterin the data)
+        print("2nnd call")
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
+        print("3rd call")
+        // when the app disappears of the screen (ex : when hme is pressed or open new app)
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
@@ -38,7 +44,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
+        // triggers when user terminate or system terminate
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        print("applicaton will terminate")
+        
     }
 
 
